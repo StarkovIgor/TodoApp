4528e0890316aca2e712915cfbd298b8d130543d
StarkovIgor <49684300+StarkovIgor@users.noreply.github.com>
2022-03-24 11:13:40 +0700

    Add files via upload

---
+++ TodoApp/TodoApp/Models/TodoModel.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        private string _text;
+        private bool _isDone;
+        [JsonProperty("creationDate")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        [JsonProperty("isDone")]
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        [JsonProperty("text")]
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
